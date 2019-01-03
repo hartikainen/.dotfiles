e9f124dcd12d77ba9b9cfc2254102f74649b1cd6
Kristian Hartikainen <hartikainen@berkeley.edu>
2019-01-03 13:15:10 -0800

    Add spotify installation

---
+++ setup/install/macos/spotify.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "./utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+
+    brew_install "Spotify" "brew install spotify"
+
+}
+
+main
---
+++ setup/install/spotify.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+
+    print_in_purple "\n   Spotify\n\n"
+
+    "./$(get_os)/spotify.sh"
+
+}
+
+main

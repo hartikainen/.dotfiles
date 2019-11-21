1ae89edc71eba826db6c33cec76b18ec3262e149
Kristian Hartikainen <hartikainen@berkeley.edu>
2019-11-21 14:35:03 -0800

    Fix ioreg commands

--- setup/preferences/macos/keyboard.sh
+++ setup/preferences/macos/keyboard.sh
@@ -8,13 +8,16 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 print_in_purple "\n   Keyboard\n\n"
 
 
-KEYBOARD_VENDOR=$(ioreg -n "Apple Internal Keyboard" -r | grep '"idVendor"' | awk '{print $4}') # 1452
-KEYBOARD_PRODUCT=$(ioreg -n "Apple Internal Keyboard" -r | grep '"idProduct"' | awk '{print $4}') # 610
-KEYBOARD_NUMBER=0 # TODO(hartikainen): Not sure if this can be hard-coded
-KEYBOARD_ID="${KEYBOARD_VENDOR}-${KEYBOARD_PRODUCT}-${KEYBOARD_ID}"
+KEYBOARD_VENDOR=$(ioreg -Src IOHIDInterface | awk '$2 == "\"VendorID\"" { print $4 }') # 1452
+KEYBOARD_PRODUCT=$(ioreg -Src IOHIDInterface | awk '$2 == "\"ProductID\"" { print $4 }') # 610
 
-execute "defaults -currentHost write -g 'com.apple.keyboard.modifiermapping.${KEYBOARD_ID}' -array-add '<dict><key>HIDKeyboardModifierMappingDst</key><integer>2</integer><key>HIDKeyboardModifierMappingSrc</key><integer>0</integer></dict>'" \
-        "Remap CAPS LOCK to CTRL"
+if [ -z "${KEYBOARD_VENDOR}" && -z "${KEYBOARD_PRODUCT}" ]; then
+    KEYBOARD_NUMBER=0 # TODO(hartikainen): Not sure if this can be hard-coded
+    KEYBOARD_ID="${KEYBOARD_VENDOR}-${KEYBOARD_PRODUCT}-${KEYBOARD_ID}"
+
+    execute "defaults -currentHost write -g 'com.apple.keyboard.modifiermapping.${KEYBOARD_ID}' -array-add '<dict><key>HIDKeyboardModifierMappingDst</key><integer>2</integer><key>HIDKeyboardModifierMappingSrc</key><integer>0</integer></dict>'" \
+            "Remap CAPS LOCK to CTRL"
+fi
 
 execute "defaults write -g AppleKeyboardUIMode -int 3" \
     "Enable full keyboard access for all controls"

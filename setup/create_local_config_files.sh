e616ff3989ac86423e843264cc11620f9d9580ca
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2019-11-22 21:23:04 +0000

    Add prompt for git name and email

--- setup/create_local_config_files.sh
+++ setup/create_local_config_files.sh
@@ -36,10 +36,26 @@ create_zsh_local() {
 create_gitconfig_local() {
 
     declare -r FILE_PATH="$HOME/.gitconfig.local"
+    declare GIT_NAME=""
+    declare GIT_EMAIL=""
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    if [ ! -e "$FILE_PATH" ] || [ -z "$FILE_PATH" ]; then
+    if [ ! -e "${FILE_PATH}" ] || [ -z "${FILE_PATH}" ]; then
+
+        ask_for_confirmation "Do you want to set git user details now?"
+        if answer_is_yes; then
+            dotfilesDirectory=""
+            while [ -z "${GIT_NAME}" ]; do
+                ask "Please specify git name (e.g. <firstname> <lastname>): "
+                GIT_NAME="$(get_answer)"
+            done
+
+            while [ -z "${GIT_EMAIL}" ]; do
+                ask "Please specify git email (e.g. <firstname>.<lastname>@<provider>.com): "
+                GIT_EMAIL="$(get_answer)"
+            done
+        fi
 
         printf "%s\n" \
 "[commit]
@@ -52,8 +68,8 @@ create_gitconfig_local() {
 
 [user]
 
-    name =
-    email =
+    name = ${GIT_NAME}
+    email = ${GIT_EMAIL}
     # signingkey =" \
         >> "$FILE_PATH"
     fi

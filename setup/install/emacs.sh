ade25f9197a19a1b97f23204ff389f428dc09bc1
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2019-11-22 22:13:49 +0000

    Create emacs conda environment in installation

--- setup/install/emacs.sh
+++ setup/install/emacs.sh
@@ -3,6 +3,7 @@
 cd "$(dirname "${BASH_SOURCE[0]}")" \
     && . "../utils.sh"
 
+declare -r LOCAL_SHELL_CONFIG_FILE="${HOME}/.bash.local"
 declare -a EMACS_SETTINGS_REMOTE="git@github.com:hartikainen/myemacs.git"
 declare -a EMACS_SETTINGS_DIR="${HOME}/.emacs.d"
 
@@ -16,6 +17,29 @@ clone_emacs_settings() {
     print_success "Emacs (clone settings)"
 }
 
+create_emacs_conda_environment() {
+
+    # Need to source this to activate conda
+    . "${LOCAL_SHELL_CONFIG_FILE}"
+
+    execute \
+        "conda create --name emacs 'python>=3.7' || true" \
+        "Emacs (create conda env)"
+
+    EMACS_CONDA_PACKAGES=(
+        "jedi"
+        "autopep8"
+        "yapf"
+        "black"
+        "flake8"
+    )
+
+    execute \
+        "conda activate emacs && pip install ${EMACS_CONDA_PACKAGES[@]}" \
+        "Emacs (install conda packages)"
+
+}
+
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 main() {
@@ -27,6 +51,8 @@ main() {
     printf "\n"
 
     clone_emacs_settings
+
+    create_emacs_conda_environment
 
 }
 

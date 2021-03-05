ce4f7d7917612ccae0177603c679cc66a0179d84
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2021-03-05 22:09:25 +0200

    Install ripgrep with homebrew fzf

--- setup/install/macos/fzf.sh
+++ setup/install/macos/fzf.sh
@@ -11,6 +11,7 @@ main() {
     print_in_purple "\n   fzf\n\n"
 
     brew_install "fzf" "fzf"
+    brew_install "ripgrep" "ripgrep"
     # To install useful key bindings and fuzzy completion:
     execute "$(brew --prefix)/opt/fzf/install --all"
 

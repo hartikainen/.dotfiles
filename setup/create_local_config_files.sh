f46644242fcb3d3f7f2cf21af451b062cb708932
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2026-05-25 19:14:34 -0400

    Fix git set up

--- setup/create_local_config_files.sh
+++ setup/create_local_config_files.sh
@@ -47,24 +47,30 @@ create_zsh_local() {
 create_gitconfig_local() {
 
     declare -r FILE_PATH="${XDG_CONFIG_HOME}/git/config.local"
-    declare GIT_NAME=""
-    declare GIT_EMAIL=""
+    declare GIT_NAME
+    GIT_NAME="$(git config --global user.name 2>/dev/null)"
+    declare GIT_EMAIL
+    GIT_EMAIL="$(git config --global user.email 2>/dev/null)"
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     if [ ! -e "${FILE_PATH}" ] || [ -z "${FILE_PATH}" ]; then
 
-        ask_for_confirmation "Do you want to set git user details now?"
-        if answer_is_yes; then
-            while [ -z "${GIT_NAME}" ]; do
-                ask "Please specify git name (e.g. <firstname> <lastname>): "
-                GIT_NAME="$(get_answer)"
-            done
+        if [ -z "${GIT_NAME}" ] || [ -z "${GIT_EMAIL}" ]; then
+            if ! $skipQuestions; then
+                ask_for_confirmation "Do you want to set git user details now?"
+                if answer_is_yes; then
+                    while [ -z "${GIT_NAME}" ]; do
+                        ask "Please specify git name (e.g. <firstname> <lastname>): "
+                        GIT_NAME="$(get_answer)"
+                    done
 
-            while [ -z "${GIT_EMAIL}" ]; do
-                ask "Please specify git email (e.g. <firstname>.<lastname>@<provider>.com): "
-                GIT_EMAIL="$(get_answer)"
-            done
+                    while [ -z "${GIT_EMAIL}" ]; do
+                        ask "Please specify git email (e.g. <firstname>.<lastname>@<provider>.com): "
+                        GIT_EMAIL="$(get_answer)"
+                    done
+                fi
+            fi
         fi
 
         printf "%s\n" \
@@ -73,15 +79,17 @@ create_gitconfig_local() {
     # Sign commits using GPG.
     # https://help.github.com/articles/signing-commits-using-gpg/
 
-    # gpgsign = true
+    # gpgsign = true" >>"${FILE_PATH}"
 
-
-[user]
+        if [ -n "${GIT_NAME}" ] && [ -n "${GIT_EMAIL}" ]; then
+            printf "\n%s\n" \
+                "[user]
 
     name = ${GIT_NAME}
     email = ${GIT_EMAIL}
-    # signingkey =" \
-            >>"${FILE_PATH}"
+    # signingkey =" >>"${FILE_PATH}"
+        fi
+
     fi
 
     print_result $? "${FILE_PATH}"
